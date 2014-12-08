d6ed321a05215c07964f97bdb5d0b256226eeff2
Eric Schrijver <eric@ericschrijver.nl>
2014-12-08 13:59:14 +0100

    En nu werkt de camera, waarschijnlijk (need to test on device)

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,27 @@
+function onSuccess(imageData) {
+    alert("Je zegt haha");
+    var $image = $("<img />");
+    $image.attr("src", "data:image/jpeg;base64," + imageData);
+    var newHahaBox = $('<div class="message haha" />');
+    newHahaBox.append($image);
+    $("#message-container").append(newHahaBox);
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
 $("#the-submit-button").on("click", function(e){
     e.preventDefault();
     var messageText = $("textarea").val();
     $("textarea").val("");
     var newChatBox = $('<div class="message">' + messageText + '</div>');
     $("#message-container").append(newChatBox);
+    if (messageText.match(/haha/i)) {
+        navigator.camera.getPicture(onSuccess, onFail,
+        { quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL
+        });
+    }
+
 });
